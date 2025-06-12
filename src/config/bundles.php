43078508ea694d470ab9fe1baa555effbd0350d7
Robert Durica <r.durica@gmail.com>
2025-06-12 23:08:50 +0200

    sentry fix

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['dev' => true],
 ];
